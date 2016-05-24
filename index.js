52a01e0f999ca7aa30943d16e668feab37576403
Computer User <computeruser@Macintosh.local>
2016-05-24 10:09:21 -0700

    copy update

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ controller.on('bot_channel_join', function (bot, message) {
 });
 
 controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello! I am bot_one');
+    bot.reply(message, 'Hello! I am bot_one, How are you?');
 });
 
 
